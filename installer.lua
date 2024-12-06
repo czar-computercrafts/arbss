509a2620c04029eeb933cb8ea9f97653fa636fb6
czar-computercrafts <czarcomputercrafts@gmail.com>
2024-12-06 09:44:41 -0700

    final installer message

--- installer.lua
+++ installer.lua
@@ -7,3 +7,5 @@ print("Getting new files")
 shell.execute("wget", "https://raw.github.com/czar-computercrafts/arbss/main/arbss.lua")
 shell.execute("wget", "https://raw.github.com/czar-computercrafts/arbss/main/guiManager.lua")
 shell.execute("wget", "https://raw.github.com/czar-computercrafts/arbss/main/peripheralManager.lua")
+
+print("\nAll Done!\nRun arbss to start, or rename arbss.lua to startup.lua to run whenever the computer resarts!")
